eccda534d1ec86100b8dcced769758881430f9bc
arloor <admin@arloor.com>
2018-08-20 14:30:31 +0800

    分片传输搞不出来惹，不搞了

--- objectdetect/src/main/java/application/DetectApplication.java
+++ objectdetect/src/main/java/application/DetectApplication.java
@@ -11,7 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,7 +29,7 @@ import java.util.List;
 
 @RestController
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 public class DetectApplication {
 
 
